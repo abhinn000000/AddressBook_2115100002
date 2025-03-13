97f7df3efdc55854f7ca0583ed2d036f8ebeb2d6
a.abhinn <abhinn000000@gmail.com>
2025-03-13 10:06:09 +0530

    [Abhinn] Add: adding files for UC2 in Branch Section1_UC2

--- AddressBook/Controllers/AddressBookController.cs
+++ AddressBook/Controllers/AddressBookController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using ModelLayer.Models;
+
 
 namespace AddressBook.Controllers
 {
@@ -6,6 +8,64 @@ namespace AddressBook.Controllers
     [Route("[controller]")]
     public class AddressBookController : ControllerBase
     {
-        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            ResponseModel<string> responseModel = new ResponseModel<string>
+            {
+                Success = true,
+                Message = "AddressBook - Fetch all contacts",
+                Data = "List of contacts displayed"
+            };
+            return Ok(responseModel);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            ResponseModel<string> responseModel = new ResponseModel<string>
+            {
+                Success = true,
+                Message = $"Contact with ID {id} fetched",
+                Data = $"Contact Details for ID: {id}"
+            };
+            return Ok(responseModel);
+        }
+
+        [HttpPost]
+        public IActionResult Post(RequestModel requestModel)
+        {
+            ResponseModel<string> responseModel = new ResponseModel<string>
+            {
+                Success = true,
+                Message = "Contact added successfully",
+                Data = $"Name: {requestModel.key}, Phone: {requestModel.value}"
+            };
+            return Ok(responseModel);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, RequestModel requestModel)
+        {
+            ResponseModel<string> responseModel = new ResponseModel<string>
+            {
+                Success = true,
+                Message = $"Contact with ID {id} updated",
+                Data = $"Updated Name: {requestModel.key}, Updated Phone: {requestModel.value}"
+            };
+            return Ok(responseModel);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            ResponseModel<string> responseModel = new ResponseModel<string>
+            {
+                Success = true,
+                Message = $"Contact with ID {id} deleted successfully",
+                Data = "Deletion successful"
+            };
+            return Ok(responseModel);
+        }
     }
 }
