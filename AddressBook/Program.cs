3d1b4797e2f53f39edd26a05548393a0a9792b64
a.abhinn <abhinn000000@gmail.com>
2025-03-19 11:02:42 +0530

    [Abhinn] Add: adding files for Section4_UC1 to branch

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,6 +5,9 @@ using BusinessLayer.Services;
 using BusinessLayer.Interface;
 using RepositoryLayer.Interface;
 using RepositoryLayer.Services;
+using BusinessLayer.Service;
+using Middleware.Authenticator;
+using Middleware.Email;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,10 +21,22 @@ builder.Services.AddControllers().AddFluentValidation(fv =>
     fv.RegisterValidatorsFromAssemblyContaining<AddressBookValidatorBL>();
 });
 
+// Configure Redis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["Redis:ConnectionString"];
+    options.InstanceName = builder.Configuration["Redis:InstanceName"];
+});
+
+
 //auto mapper
 builder.Services.AddAutoMapper(typeof(AutoMapperProfileBL));
 builder.Services.AddScoped<IAddressBookBL, AddressBookBL>();
 builder.Services.AddScoped<IAddressBookRL, AddressBookRL>();
+builder.Services.AddScoped<JwtTokenService>();
+builder.Services.AddScoped<EmailService>();
+builder.Services.AddScoped<ICacheService, CacheService>();
+builder.Services.AddSwaggerGen();
 
 
 // Add services to the container.
@@ -29,6 +44,11 @@ builder.Services.AddScoped<IAddressBookRL, AddressBookRL>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+//Add swagger to container
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
